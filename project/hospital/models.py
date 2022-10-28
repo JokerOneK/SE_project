b54b0072c1cd50e7846c790696d187f579e795d0
Altay Toktassyn <altosha211@gmail.com>
2022-10-28 16:09:05 +0600

    Added information to models.py

--- project/hospital/models.py
+++ project/hospital/models.py
@@ -9,23 +9,44 @@ default = [('Cardiologist','Cardiologist'),
            ('Colon and Rectal Surgeons','Colon and Rectal Surgeons')
            ]
 
+default_categories = [('Highest', 'Highest'), ('First', 'First'), ('Second', 'Second')]
+default_degree = [('PhD', 'PhD'), ('MD', 'MD'), ('Bachelor', 'Bachelor')]
+
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=13)
-    department = models.CharField(max_length=50, choices=default, default='Dermatologists')
-
-
-class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
     middle_name = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=13)
-    email = models.EmailField()
+    department = models.CharField(max_length=50, choices=default, default='Dermatologists')
+    date_of_birth = models.DateField()
+    iin = models.CharField(max_length=12)
+    experience = models.IntegerField()
+    #photo = models.ImageField()
+    category = models.CharField(max_length=50, choices=default_categories, default='First')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    schedule_details = models.CharField(max_length=200)
+    degree = models.CharField(max_length=15, choices=default_degree, default='Bachelor')
+    rating = models.PositiveIntegerField()
+    homepage_url = models.CharField(max_length=200, null=True)
+
+
+class Patient(models.Model):
+    date_of_birth = models.DateField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    iin = models.CharField(max_length=12)
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=20)
+    blood_group = models.CharField(max_length=14, blank=True, default=None)
+    emergency_phone_number = models.CharField(max_length=13)
+    address = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13)
+    email = models.EmailField(blank=True)
     contact_details = models.CharField(max_length=200)
+    is_married = models.BooleanField(default=False)
 
 
 # Create your models here.
--- project/project/settings.py
+++ project/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,10 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
+    'hospital',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,9 +128,41 @@ STATIC_URL = '/static/'
 # REST FRAMEWORK
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
