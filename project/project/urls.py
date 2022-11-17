fb9ea154a2f012409245e8bfcb131bb80004e0a6
Altay Toktassyn <altosha211@gmail.com>
2022-11-17 17:23:46 +0600

    Added admintest

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("hospital.urls"))
+    path('api/', include("hospital.urls")),
+    path('admintest/', admin.site.urls),
 ]
