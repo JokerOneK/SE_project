3cb8075a70d4a3b9f6982f4ca4d8d5f2c74aba90
Altay Toktassyn <altosha211@gmail.com>
2022-11-19 19:02:02 +0600

    Added appoinments and API

--- project/hospital/serializer.py
+++ project/hospital/serializer.py
@@ -4,16 +4,26 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import *
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        # Add custom claims
+
         token['username'] = user.username
         token['email'] = user.email
-        # ...
+        doctor_users = Doctor.objects.filter(user=user)
+        if len(doctor_users) > 0:
+            token['role'] = 'doctor'
+        patients_users = Patient.objects.filter(user=user)
+        if len(patients_users) > 0:
+            token['role'] = 'patient'
+
+        if len(doctor_users) == 0 and len(patients_users) == 0:
+            token['role'] = 'not_found'
+
         return token
 
 
